b93c6a7da075886d6e53e381c97938bc374d629a
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-12-16 15:17:24 +0200

    finish connect

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import * as url from 'url'
+import path from 'path';
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 import { indexRouter } from './routes/router.js'
 
 
@@ -7,6 +10,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+const publicPath = path.join(__dirname, 'build')
+app.use(express.static(publicPath))
 app.use('/api', indexRouter)
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build',index.html ))
+})
 
 export { app };
