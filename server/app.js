9dc5c6f9bbca98d390f2b82dbfe22223b9edd067
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-12-18 11:01:20 +0200

    push

--- server/app.js
+++ server/app.js
@@ -1,20 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-import * as url from 'url'
+/** @format */
+
+import express from 'express';
+import cors from 'cors';
+import * as url from 'url';
 import path from 'path';
 const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
-import { indexRouter } from './routes/router.js'
-
+import { indexRouter } from './routes/router.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-const publicPath = path.join(__dirname,'server', 'build')
-app.use(express.static(publicPath))
-app.use('/api', indexRouter)
+const publicPath = path.join(__dirname, 'server', 'build');
+app.use(express.static(publicPath));
+app.use('/api', indexRouter);
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'server', 'build',index.html ))
-})
+	res.sendFile(path.resolve(__dirname, 'server', 'build'));
+});
 
 export { app };
